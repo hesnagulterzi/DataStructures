e3a7c5d6e9a77486c1b49275784dfd90470ee7bd
hesnaterzi <hesnaterzi@gmail.com>
2021-03-29 00:44:39 +0300

    First Commit

---
+++ src/main/java/com/hesna/datastructures/DatastructuresApplication.java
@@ -0,0 +1,13 @@
+package com.hesna.datastructures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatastructuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatastructuresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hesna/datastructures/DatastructuresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hesna.datastructures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatastructuresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
